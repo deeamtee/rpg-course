f9a2ba605aba0fcaf92b066f73f46914ec7099ba
Sergey K. <mock@email.zxc>
2024-01-09 11:29:44 +0300

    #1 Создание карты в Tilemap Editor

--- src/scenes/durotar.ts
+++ src/scenes/durotar.ts
@@ -1,1 +1,20 @@
-export class Durotar {}
+import durotarJSON from '../assets/durotar.json';
+import { LAYERS, SIZES, TILES } from '../utils/constants';
+
+export class Durotar extends Phaser.Scene {
+    constructor() {
+        super('DurotarScene');
+    }
+
+    preload () {
+        this.load.image(TILES.DUROTAR, 'src/assets/durotar.png')
+        this.load.tilemapTiledJSON('map', 'src/assets/durotar.json')
+    }
+
+    create () {
+        const map = this.make.tilemap({ key: "map" });
+        const tileset = map.addTilesetImage(durotarJSON.tilesets[0].name, TILES.DUROTAR, SIZES.TILE, SIZES.TILE);
+        const groundLayer = map.createLayer(LAYERS.GROUND, tileset, 0, 0);
+        const wallsLayer = map.createLayer(LAYERS.WALLS, tileset, 0, 0);
+    }
+}
---
+++ src/utils/constants.ts
@@ -0,0 +1,12 @@
+export const TILES = {
+    DUROTAR: 'durotar'
+}
+
+export const SIZES = {
+    TILE: 32
+}
+
+export const LAYERS = {
+    GROUND: 'ground',
+    WALLS: 'walls'
+}
